0a4b1c70d756f425d243ab8ac26a1832e4bd62de
Tobias Büser <germansuperioz@yahoo.de>
2026-01-10 23:55:52 +0100

    wip: work on search bar

---
+++ src/components/dlc/IconDropdown/IconDropdown.tsx
@@ -0,0 +1,112 @@
+import { IconCheck } from '@tabler/icons-react';
+import { type JSX, useEffect, useRef, useState } from 'react';
+import { useWindowSize } from '@/hooks/useWindowSize.ts';
+import styles from './IconDropdown.module.css';
+
+type IconDropdownProps = {
+  items: Record<string, string>;
+  dynamicIcons?: boolean;
+  icons: Record<string, JSX.Element>;
+  defaultSelected?: string;
+  onSelect?: (selected: string) => void;
+};
+
+export default function IconDropdown(props: IconDropdownProps) {
+  const [open, setOpen] = useState(false);
+
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
+  const contentRef = useRef<HTMLDivElement | null>(null);
+
+  const shouldOpenRight = (ref: HTMLDivElement | null, windowWidth: number): boolean => {
+    if (!ref) return false;
+
+    const isVisible = ref.getBoundingClientRect().right <= windowWidth;
+    return !isVisible;
+  };
+
+  const [selected, setSelected] = useState<string | null>(props.defaultSelected || null);
+
+  const toggle = () => {
+    const newOpen = !open;
+
+    setOpen(newOpen);
+    buttonRef.current?.setAttribute('active', `${newOpen}`);
+    contentRef.current?.setAttribute('active', `${newOpen}`);
+
+    const right = shouldOpenRight(contentRef.current, window.innerWidth);
+    if (newOpen && right) {
+      contentRef.current?.setAttribute('right', 'true');
+    } else {
+      contentRef.current?.setAttribute('right', 'false');
+    }
+  };
+
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (open && !dropdownRef.current?.contains(event.target as Element)) {
+        toggle();
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  });
+
+  const [width] = useWindowSize();
+  const [previousWidth, setPreviousWidth] = useState(0);
+
+  // TODO: debounce, but idgaf
+  // biome-ignore lint/correctness/useExhaustiveDependencies: idgaf
+  useEffect(() => {
+    const newWidth = width;
+    const isAlreadyRight = contentRef.current?.getAttribute('right') === 'true';
+
+    setPreviousWidth(newWidth);
+
+    if (newWidth < previousWidth && isAlreadyRight) {
+      return;
+    }
+    if (newWidth > previousWidth && !isAlreadyRight) {
+      return;
+    }
+
+    const right = shouldOpenRight(contentRef.current, width);
+    if (right) {
+      contentRef.current?.setAttribute('right', 'true');
+    } else {
+      contentRef.current?.setAttribute('right', 'false');
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [width]);
+
+  return (
+    <div className={styles.dropdown} ref={dropdownRef}>
+      <button type="button" ref={buttonRef} className={styles.dropdownButton} onClick={() => toggle()}>
+        {(props.dynamicIcons ?? true) && <>{props.icons[selected || '']}</>}
+        {props.dynamicIcons === false && Object.values(props.icons)[0]}
+      </button>
+      <div className={styles.dropdownContent} ref={contentRef}>
+        {Object.entries(props.items).map(([key, label]) => (
+          // biome-ignore lint/a11y/useKeyWithClickEvents: idgaf
+          // biome-ignore lint/a11y/noStaticElementInteractions: idgaf
+          <div
+            key={key}
+            onClick={() => {
+              setSelected(key);
+              props.onSelect?.(key);
+
+              toggle();
+            }}
+          >
+            <p style={selected === key ? { color: 'white' } : {}}>{label}</p>
+            {selected === key && <IconCheck color={'white'} size={18} />}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/components/home/Navbar/Navbar.tsx
+++ src/components/home/Navbar/Navbar.tsx
@@ -1,6 +1,24 @@
 import { useMediaQuery } from '@mantine/hooks';
-import { IconMenu2, IconMoon, IconSearch, IconUser } from '@tabler/icons-react';
+import {
+  IconArrowBack,
+  IconArrowDown,
+  IconArrowUp,
+  IconChevronDown,
+  IconClockHour8,
+  IconLanguage,
+  IconMenu2,
+  IconMoon,
+  IconQuestionMark,
+  IconSearch,
+  IconStar,
+  IconSun,
+  IconSunMoon,
+  IconUser,
+  IconX,
+} from '@tabler/icons-react';
 import { Link } from '@tanstack/react-router';
+import IconDropdown from '@/components/dlc/IconDropdown/IconDropdown.tsx';
+import { DLCIcon } from '@/helpers/icons/games/dlc/Icon.tsx';
 import { Logo } from '@/helpers/icons/Logo.tsx';
 import styles from './Navbar.module.css';
 
@@ -20,13 +38,103 @@ export default function Navbar({ setSidebarOpen }: NavbarProps) {
             <p>Dokumentation</p>
           </div>
 
+          <div className={styles.searchOverlay} />
           <div className={styles.navbarSearch}>
-            <p>Search</p>
-            <p>?</p>
+            <div className={styles.searchbar}>
+              <IconSearch size={20} color={'#9ba6b1'} className={styles.searchIcon} />
+              <input
+                type="text"
+                onFocus={() => {
+                  // TODO: display div around
+
+                  console.log('Focus!');
+                }}
+              />
+              <button type="button">
+                <IconQuestionMark size={18} color={'#9ba6b1'} />
+              </button>
+              <div className={styles.searchModal}>
+                <div className={styles.content}>
+                  <div className={styles.gameSelector}>
+                    <DLCIcon width={20} height={20} color={'#9ba6b1'} />
+                    <p>DISNEY LORCANA</p>
+                    <IconChevronDown size={18} color={'#9ba6b1'} />
+                  </div>
+                  <div className={styles.recent}>
+                    <p>ZULETZT</p>
+                    <ul>
+                      {[
+                        'ink:amber and type:hero',
+                        'name:mickey name:mouse oracle:wunder oracle:haus ink:steel is:inkwell',
+                        'ability="Deep Freeze" and o:"chosen characters"',
+                        'name:mickey name:mouse oracle:wunder oracle:haus ink:steel is:inkwell',
+                        'ability="Deep Freeze" and o:"chosen characters"',
+                      ].map((query, index) => (
+                        <li key={index}>
+                          <div className={styles.recentItemLeft}>
+                            <IconClockHour8 size={22} color={'#9ba6b1'} />
+                            <p>{query}</p>
+                          </div>
+                          <div className={styles.recentItemRight}>
+                            <IconStar size={16} color={'#9ba6b1'} />
+                            <IconX size={16} color={'#9ba6b1'} />
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+
+                <div className={styles.footer}>
+                  <div className={styles.controls}>
+                    <div>
+                      <p>Navigieren</p>
+                      <kbd>
+                        <IconArrowUp size={18} color={'#9ba6b1'} />
+                      </kbd>
+                      <kbd>
+                        <IconArrowDown size={18} color={'#9ba6b1'} />
+                      </kbd>
+                    </div>
+                    <div>
+                      <p>Suche starten</p>
+                      <kbd>
+                        <IconArrowBack size={18} color={'#9ba6b1'} />
+                      </kbd>
+                    </div>
+                    <div>
+                      <p>Schließen</p>
+                      <kbd>esc</kbd>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
 
           <div className={styles.navbarRight}>
-            <IconMoon size={18} color={'#9ba6b1'} />
+            <IconDropdown
+              items={{
+                de: 'Deutsch',
+                en: 'English',
+              }}
+              icons={{ _: <IconLanguage size={22} color={'#9ba6b1'} /> }}
+              defaultSelected={'de'}
+              dynamicIcons={false}
+            />
+            <IconDropdown
+              icons={{
+                dark: <IconMoon size={22} color={'#9ba6b1'} />,
+                light: <IconSun size={22} color={'#9ba6b1'} />,
+                system: <IconSunMoon size={22} color={'#9ba6b1'} />,
+              }}
+              items={{
+                dark: 'Dark Mode',
+                light: 'Light Mode',
+                system: 'Auto',
+              }}
+              defaultSelected={'dark'}
+            />
             <button type="button">Anmelden / Registrieren</button>
           </div>
         </nav>
