182bf4c7b327c5cd57459af96b328d5307f0b488
Tobias Büser <germansuperioz@yahoo.de>
2026-03-08 11:55:55 +0100

    feat: display search in list

--- src/parcels/lists/ListsOverview/ListRenderer/ListRenderer.tsx
+++ src/parcels/lists/ListsOverview/ListRenderer/ListRenderer.tsx
@@ -1,5 +1,5 @@
-import {Center, Group, Stack} from '@mantine/core';
-import {IconCards, IconSearch} from '@tabler/icons-react';
+import {Center, Group, Stack, Tooltip, UnstyledButton} from '@mantine/core';
+import {IconAlertTriangleFilled, IconCards, IconCircleCheckFilled, IconGauge, IconSearch,} from '@tabler/icons-react';
 import {useTranslation} from 'react-i18next';
 import type {TcgDataCard} from '@/parcels/details/TcgPrintDetails/TcgPrintDetails.tsx';
 import {formatRelativeTimestamp} from '@/parcels/lists/ListsOverview/formatRelativeTimestamp.ts';
@@ -8,6 +8,7 @@ import type {ResolvedUserListResource, UserListWithResources} from '@/parcels/li
 import {GourmetText} from '@/parcels/mantine/GourmetText.tsx';
 import {createProps} from '@/parcels/overview/CardGrid/ImageCardWithSelection/createProps.ts';
 import {ImageCard} from '@/parcels/overview/ImageCard/ImageCard.tsx';
+import type {UserResolvedSavedSearch} from '@/parcels/search/types.ts';
 import type {TcgSearchDataCard} from '@/parcels/tcg/types.ts';
 import type {Tcg} from '@/parcels/tcg/useTcgByLocation.ts';
 import styles from './ListRenderer.module.css';
@@ -54,7 +55,7 @@ export function ListRenderer({ tcg, listWithResources }: { tcg: Tcg; listWithRes
               </Group>
             )}
             {cardResources.length > 0 && (
-              <Group wrap={'nowrap'} h={'5rem'}>
+              <Group wrap={'nowrap'} h={'7.5rem'}>
                 <div style={{ justifySelf: 'start', height: '100%', flexShrink: 0, color: list.color ?? '' }}>
                   <Center className={styles.resourceIcon}>
                     <IconCards size={20} />
@@ -72,13 +73,82 @@ export function ListRenderer({ tcg, listWithResources }: { tcg: Tcg; listWithRes
 
 function SearchResourcesRenderer({ resources }: { tcg: Tcg; resources: ResolvedUserListResource[] }) {
   return (
-    <Stack style={{ wordBreak: 'break-all', height: '100%', overflowY: 'hidden' }}>
-      {resources.map((resource) => (
-        <Group key={resource.listResource.resourceId} h={'1.5rem'}>
-          <div>{JSON.stringify(resource)}</div>
-        </Group>
-      ))}
+    <Stack
+      style={{
+        height: '100%',
+        width: '100%',
+        overflowY: 'hidden',
+      }}
+    >
+      {resources.map((resource) => {
+        const search = resource.resourceData as unknown as UserResolvedSavedSearch;
+
+        return (
+          <UnstyledButton
+            key={resource.listResource.resourceId}
+            h={'2rem'}
+            w={'100%'}
+            className={styles.searchButton}
+            onClick={() => {
+              // TODO: execute query (maybe we can use Link as well for that)
+            }}
+          >
+            <Group h={'100%'} w={'100%'} justify={'space-between'}>
+              <Tooltip label={search.search.rawQuery} openDelay={1000}>
+                <GourmetText
+                  cgmff={'monospace'}
+                  fz={'0.9rem'}
+                  maw={'24rem'}
+                  style={{
+                    textWrap: 'nowrap',
+                    overflowX: 'hidden',
+                    textOverflow: 'ellipsis',
+                  }}
+                >
+                  {search.search.rawQuery}
+                </GourmetText>
+              </Tooltip>
+
+              <Group h={'100%'} gap={'0.5rem'}>
+                <GourmetText cgmff={'ui'}>{search.search.results} cards</GourmetText>
+                <SpeedGauge execTime={search.search.executionTime} />
+              </Group>
+            </Group>
+          </UnstyledButton>
+        );
+      })}
     </Stack>
+  );
+}
+
+function SpeedGauge({ execTime }: { execTime: number }) {
+  let color: string;
+  if (execTime <= 100) {
+    color = 'var(--gourmet-green-1)';
+  } else if (execTime <= 1000) {
+    color = 'var(--gourmet-orange-1)';
+  } else {
+    color = 'var(--gourmet-red-01)';
+  }
+
+  return (
+    <Tooltip label={`Query took ${execTime}ms server-side`} openDelay={1000}>
+      <div
+        style={{
+          position: 'relative',
+          width: 20,
+          height: 20,
+          display: 'inline-block',
+        }}
+      >
+        <IconGauge color={color} size={20} />
+        {execTime <= 100 && <IconCircleCheckFilled size={12} color={color} className={styles.speedIcon} />}
+        {execTime > 100 && execTime <= 1000 && (
+          <IconCircleCheckFilled size={12} color={color} className={styles.speedIcon} />
+        )}
+        {execTime > 1000 && <IconAlertTriangleFilled size={12} color={color} className={styles.speedIcon} />}
+      </div>
+    </Tooltip>
   );
 }
 
---
+++ src/parcels/search/types.ts
@@ -0,0 +1,4 @@
+import type {components as c} from '@/schema/api';
+
+export type UserSavedSearch = c['schemas']['UserSavedSearch'];
+export type UserResolvedSavedSearch = c['schemas']['ResolvedUserSavedSearch'];
