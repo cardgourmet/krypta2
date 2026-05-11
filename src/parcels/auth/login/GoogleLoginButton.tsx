79c0c2fa452a3f801f5cf2e3338eaf08f341a2a2
Tobias Büser <germansuperioz@yahoo.de>
2026-05-11 13:08:38 +0200

    feat: add login with google

--- src/parcels/auth/login/GoogleLoginButton.tsx
+++ src/parcels/auth/login/GoogleLoginButton.tsx
@@ -1,8 +1,12 @@
-import {Button} from '@mantine/core';
-import {GoogleOAuthProvider} from '@react-oauth/google';
+import {Button, Group, Loader, Stack} from '@mantine/core';
+import {GoogleOAuthProvider, useGoogleLogin} from '@react-oauth/google';
 import {IconBrandGoogleFilled} from '@tabler/icons-react';
-import {useState} from 'react';
+import {useNavigate} from '@tanstack/react-router';
+import {startTransition, useState} from 'react';
+import {useAuth} from '@/parcels/auth/AuthContext.ts';
+import {loginUsingOAuth} from '@/parcels/auth/api.ts';
 import {GourmetText} from '@/parcels/generic/mantine/GourmetText.tsx';
+import {Route} from '@/routes/login';
 
 export function GoogleLoginButton() {
   return (
@@ -13,19 +17,77 @@ export function GoogleLoginButton() {
 }
 
 function GoogleLoginButtonIntegration() {
+  const { login } = useAuth();
+  const { redirect } = Route.useSearch();
+
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>('');
 
+  const navigate = useNavigate();
+  const googleLogin = useGoogleLogin({
+    onSuccess: (res) => {
+      const date = new Date();
+      date.setSeconds(date.getSeconds() + res.expires_in);
+
+      startTransition(async () => {
+        const r = await loginUsingOAuth(res.access_token, date.toISOString(), 'google');
+        setLoading(false);
+
+        if (r.error) {
+          setError(r.error.key);
+          return;
+        }
+
+        // is logged in
+        if ((r.session && r.data?.session) || r.data?.user?.state === 'unverified') {
+          login({
+            token: r.session,
+            expiresAt: r.data?.session.expiresAt,
+            user: r.data.user,
+          });
+        }
+
+        // noinspection JSIgnoredPromiseFromCall
+        navigate({
+          to: redirect ?? '/',
+          replace: true,
+        });
+      });
+    },
+    onError: (err) => {
+      setLoading(false);
+
+      setError('oauth-error');
+      console.log('error during oauth flow', err);
+    },
+    onNonOAuthError: (err) => {
+      setLoading(false);
+
+      setError('unknown');
+      console.log('unexpected error', err);
+    },
+    scope: 'https://www.googleapis.com/auth/userinfo.email',
+  });
+
   return (
-    <Button
-      leftSection={<IconBrandGoogleFilled color={'var(--gourmet-neutral-0)'} />}
-      color={'var(--gourmet-neutral-9)'}
-      onClick={() => {
-        // TODO: first call useLogin from google
-        // TODO: call loginUsingOAuth
-      }}
-    >
-      <GourmetText c={'var(--gourmet-neutral-1)'}>Continue with Google</GourmetText>
-    </Button>
+    <Stack>
+      <Button
+        leftSection={<IconBrandGoogleFilled color={'var(--gourmet-neutral-0)'} />}
+        color={'var(--gourmet-neutral-9)'}
+        onClick={() => {
+          googleLogin();
+        }}
+      >
+        <Group>
+          <GourmetText c={'var(--gourmet-neutral-1)'}>Continue with Google</GourmetText>
+          {loading && <Loader size={18} />}
+        </Group>
+      </Button>
+      {error && (
+        <GourmetText c={'var(--gourmet-red-01)'} fz={'0.95rem'}>
+          {error}
+        </GourmetText>
+      )}
+    </Stack>
   );
 }
