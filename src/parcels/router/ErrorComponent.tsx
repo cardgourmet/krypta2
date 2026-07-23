6d637015ac90294f2f63a9f6d41d6a697e98eb09
Tobias Büser <germansuperioz@yahoo.de>
2026-07-23 16:57:52 +0200

    chore: add error component

---
+++ src/parcels/router/ErrorComponent.tsx
@@ -0,0 +1,12 @@
+import { Stack } from '@mantine/core';
+import { GourmetText } from '@/parcels/generic/mantine/GourmetText.tsx';
+
+export function ErrorComponent({ error }: { error: unknown }) {
+  const message = error instanceof Error ? error.message : 'Unknown :(';
+
+  return (
+    <Stack>
+      <GourmetText>There has been error: {message}</GourmetText>
+    </Stack>
+  );
+}
