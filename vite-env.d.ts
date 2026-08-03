727e528c1ee122ec4a79db2fb78f7c258b92c42e
Tobias Büser <germansuperioz@yahoo.de>
2026-08-03 18:05:51 +0200

    fix: something

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_API_BASE_URL: string;
   readonly VITE_OAUTH_GOOGLE_CLIENT_ID: string;
+  readonly VITE_CARDGOURMET_BASIC_USERNAME: string;
+  readonly VITE_CARDGOURMET_BASIC_PASSWORD: string;
 }
 
 interface ImportMeta {
