dc8fb9856a8f8d1619cbf0c2b02f69174564dbd1
vainamov <krahtz@neoskop.de>
2026-09-26 16:55:32 +0200

    feat: add sidecar

--- src/parcels/layout/AppFrame/AppFrame.tsx
+++ src/parcels/layout/AppFrame/AppFrame.tsx
@@ -1,8 +1,12 @@
 import { nprogress } from '@mantine/nprogress';
 import { useRouter } from '@tanstack/react-router';
+import clsx from 'clsx';
+import { use } from 'react';
 import { WorkMenu } from '@/parcels/homepage/Sidebar/WorkMenu/WorkMenu';
 import { useIsOnOverview } from '@/parcels/overview/cards/CardOverview/useIsOnOverview';
+import { CurrentSearchSidecar } from '@/parcels/search/CurrentSearchSidecar/CurrentSearchSidecar';
 import { useOverviewWorkStore } from '@/parcels/selection/useOverviewWorkStore';
+import { SidecarContext } from '@/parcels/sidecar/Sidecar.context';
 import { Footer } from '../Footer/Footer';
 import { Navbar } from '../Navbar/Navbar';
 import { Sidebar } from '../Sidebar/Sidebar';
@@ -11,6 +15,7 @@ import type { AppFrameProps } from './types';
 
 export const AppFrame = ({ children }: AppFrameProps) => {
   const router = useRouter();
+  const sidecar = use(SidecarContext);
 
   router.subscribe('onBeforeLoad', ({ fromLocation, pathChanged }) => {
     if (fromLocation && pathChanged) nprogress.start();
@@ -25,17 +30,23 @@ export const AppFrame = ({ children }: AppFrameProps) => {
   const isWorkActive = workQuerySettings !== undefined;
 
   return (
-    <div className={styles.base}>
+    <div className={clsx(styles.base, sidecar.isActive && styles.sidecarInline)}>
       <Navbar className={styles.mainNavbar} />
       <Sidebar className={styles.sideNavbar} />
 
       {!isOnOverview && isWorkActive && <WorkMenu />}
 
+      <aside className={clsx(styles.sidecarHost)}>
+        <div className={clsx(styles.sidecarContainer, sidecar.isActive && styles.isActive)}>
+          <CurrentSearchSidecar />
+        </div>
+      </aside>
+
       <main className={styles.content}>
         <div className={styles.transitionPageWrapper}>{children}</div>
       </main>
 
-      <Footer />
+      <Footer className={styles.footer} />
     </div>
   );
 };
--- src/parcels/layout/Sidebar/Sidebar.tsx
+++ src/parcels/layout/Sidebar/Sidebar.tsx
@@ -1,16 +1,20 @@
+import { IconZoomScan } from '@tabler/icons-react';
 import { Link } from '@tanstack/react-router';
 import { clsx } from 'clsx';
+import { use } from 'react';
 import type { Extend, Structure } from '@/parcels/composition/extend';
+import { ActionButton } from '@/parcels/generic/ActionButton/ActionButton';
 import { Badge } from '@/parcels/generic/Badge/Badge';
-import { WorkMenuButton } from '@/parcels/homepage/Sidebar/WorkMenu/WorkMenuButton';
 import { Logo } from '@/parcels/Logo';
 import { useIsOnOverview } from '@/parcels/overview/cards/CardOverview/useIsOnOverview';
 import { useOverviewWorkStore } from '@/parcels/selection/useOverviewWorkStore';
+import { SidecarContext } from '@/parcels/sidecar/Sidecar.context';
 import { TcgSidebarItem } from '@/parcels/tcg/TcgSidebarItem';
 import styles from './Sidebar.module.css';
 
 export const Sidebar = ({ className }: Extend<Structure>) => {
   const isOnOverview = useIsOnOverview();
+  const sidecar = use(SidecarContext);
   const workMeta = useOverviewWorkStore((state) => state.data?.meta);
 
   return (
@@ -21,17 +25,24 @@ export const Sidebar = ({ className }: Extend<Structure>) => {
         </Link>
       </div>
 
-      {!isOnOverview && (workMeta?.rawElements.length ?? 0) > 0 && (
-        <div className={styles.contextContainer}>
-          <WorkMenuButton />
-        </div>
-      )}
-
       <div className={styles.tcgsContainer}>
         <TcgSidebarItem tcg="mtg" />
         <TcgSidebarItem tcg="pcg" />
         <TcgSidebarItem tcg="dlc" />
       </div>
+
+      {!isOnOverview && (workMeta?.rawElements.length ?? 0) > 0 && (
+        <div className={styles.contextContainer}>
+          <ActionButton
+            accent="complementary"
+            className={styles.sidecarButton}
+            onClick={() => sidecar.setActive(!sidecar.isActive)}
+            variant={sidecar.isActive ? 'primary' : 'secondary'}
+          >
+            <IconZoomScan />
+          </ActionButton>
+        </div>
+      )}
 
       <div className={styles.brandContainer}>
         <div className={styles.brand}>
